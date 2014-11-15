0ae3fdaff84cb418317f89da943514c079f4de72
ccleung <clemsquared@gmail.com>
2014-11-15 09:31:57 -0500

    refactor some code

--- tiltmaze.py
+++ tiltmaze.py
@@ -1,3 +1,4 @@
+from position import Position
 from enum import Enum
 import Queue
 
@@ -6,30 +7,6 @@ class Direction(Enum):
 	up = 2
 	right = 3
 	down = 4
-
-class Position(object):
-	def __init__(self, x=0, y=0, prev_position = None):
-		self.x = x
-		self.y = y
-		# don't set prev if it is ourselves, end up in an endless loop!
-		self._prev_position = prev_position
-
-	@property 
-	def prev_position(self):
-		return self._prev_position
-
-	@prev_position.setter
-	def prev_position(self, p):
-		if p is not None and p.x == self.x and p.y == self.y:
-			print "Don't set the prev position if we didn't move!"
-			#self._prev_position = None
-		else:
-			self._prev_position = p
-
-	def __str__(self):
-		return "Position %s %s and Parent: %s %s" % (self.x, self.y, 
-														self.prev_position.x if self.prev_position is not None else 'None', 
-														self.prev_position.y if self.prev_position is not None else 'None')
 
 '''
 'o' is an open position, 'x' is a wall within the maze
